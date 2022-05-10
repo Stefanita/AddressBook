1effe4d7a0989f32baa5a330e3126802b46d8644
Stefanita <tstefanita2002@gmail.com>
2022-05-10 11:14:25 +0300

    Add project files.

---
+++ ASE/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[ContactID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Company] NVARCHAR(100) NULL, 
+    [Email] NVARCHAR(100) NULL, 
+    [Telephone] NVARCHAR(100) NULL, 
+    [IsCustomer] BIT NULL, 
+    [LastCall] DATETIME NULL 
+)
